c460375deb0132dbcf15f17baa0f6916056da859
ngocdu <ngocdu171@gmail.com>
2021-04-17 14:04:02 +0300

    part 3 - creating a login with Firebase Authentication

--- app/src/main/java/din19sp/dupham/chatbox/MainActivity.kt
+++ app/src/main/java/din19sp/dupham/chatbox/MainActivity.kt
@@ -3,12 +3,19 @@ package din19sp.dupham.chatbox
 import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.InputType
 import android.util.Log
 import android.view.KeyEvent
 import android.view.inputmethod.InputMethodManager
 import android.widget.EditText
+import android.widget.LinearLayout
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.ktx.auth
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.DatabaseReference
@@ -19,10 +26,14 @@ import kotlin.math.log
 
 class MainActivity : AppCompatActivity() {
 
+    private val TAG: String = MainActivity::class.java.name
     private lateinit var messages: ArrayList<String>
     private lateinit var database: DatabaseReference
+    private lateinit var auth: FirebaseAuth
+
     private lateinit var edMessage: EditText
     private lateinit var rcMessageList: RecyclerView
+    private var currentUser: FirebaseUser? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -32,6 +43,8 @@ class MainActivity : AppCompatActivity() {
         rcMessageList = findViewById(R.id.messageList)
 
         database = Firebase.database.reference
+        auth = Firebase.auth
+
         messages = arrayListOf()
 
         edMessage.setOnKeyListener {v, keyCode, event ->
@@ -78,5 +91,52 @@ class MainActivity : AppCompatActivity() {
             val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
             imm.hideSoftInputFromWindow(view.windowToken, 0)
         }
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        loginDialog()
+    }
+
+    private fun loginDialog() {
+        val builder = AlertDialog.Builder(this)
+
+        with(builder) {
+            setTitle("Login")
+            val linearLayout: LinearLayout = LinearLayout(this@MainActivity)
+            linearLayout.orientation = LinearLayout.VERTICAL
+
+            val inputEmail: EditText = EditText(this@MainActivity)
+            inputEmail.inputType = InputType.TYPE_CLASS_TEXT or InputType.TYPE_TEXT_FLAG_NO_SUGGESTIONS or InputType.TYPE_TEXT_VARIATION_EMAIL_ADDRESS
+            inputEmail.hint = "Enter email"
+            linearLayout.addView(inputEmail)
+
+            val inputPassword: EditText = EditText(this@MainActivity)
+            inputPassword.inputType = InputType.TYPE_CLASS_TEXT or InputType.TYPE_TEXT_FLAG_NO_SUGGESTIONS or InputType.TYPE_TEXT_VARIATION_PASSWORD
+            inputPassword.hint = "Enter password"
+            linearLayout.addView(inputPassword)
+            builder.setView(linearLayout)
+
+            builder.setPositiveButton("OK") {dialog, which ->
+                login(inputEmail.text.toString(), inputPassword.text.toString())
+            }.show()
+        }
+    }
+
+    private fun login(email: String, password: String) {
+        auth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    Log.d(TAG, "signInWithEmail: Successfully!")
+                    Toast.makeText(baseContext, "signInWithEmail: Successfully!", Toast.LENGTH_SHORT).show()
+                    currentUser = auth.currentUser
+                }
+                else {
+                    Log.w(TAG, "signInWithEmail: Fail!", task.exception)
+                    Toast.makeText(baseContext, "Authentication failed", Toast.LENGTH_SHORT).show()
+                }
+            }
+
     }
 }
