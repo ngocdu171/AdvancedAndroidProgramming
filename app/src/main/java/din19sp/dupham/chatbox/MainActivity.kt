240f4f8b83376b408e2a8db38f5657b8ebbaa4f8
ngocdu <ngocdu171@gmail.com>
2021-04-30 11:52:44 +0300

    part 4

--- app/src/main/java/din19sp/dupham/chatbox/MainActivity.kt
+++ app/src/main/java/din19sp/dupham/chatbox/MainActivity.kt
@@ -1,11 +1,15 @@
 package din19sp.dupham.chatbox
 
 import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.LocaleList
 import android.text.InputType
 import android.util.Log
 import android.view.KeyEvent
+import android.view.Menu
+import android.view.MenuItem
 import android.view.inputmethod.InputMethodManager
 import android.widget.EditText
 import android.widget.LinearLayout
@@ -22,17 +26,20 @@ import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.ValueEventListener
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
-import kotlin.math.log
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+import java.util.*
+import kotlin.collections.ArrayList
+import kotlin.collections.HashMap
 
 class MainActivity : AppCompatActivity() {
 
-    private val TAG: String = MainActivity::class.java.name
-    private lateinit var messages: ArrayList<String>
-    private lateinit var database: DatabaseReference
-    private lateinit var auth: FirebaseAuth
-
     private lateinit var edMessage: EditText
     private lateinit var rcMessageList: RecyclerView
+    private val TAG: String = MainActivity::class.java.name
+    private lateinit var messages: ArrayList<Message>
+    private lateinit var database: DatabaseReference
+    private lateinit var auth: FirebaseAuth
     private var currentUser: FirebaseUser? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -41,11 +48,9 @@ class MainActivity : AppCompatActivity() {
 
         edMessage = findViewById(R.id.messageText)
         rcMessageList = findViewById(R.id.messageList)
-
         database = Firebase.database.reference
         auth = Firebase.auth
-
-        messages = arrayListOf()
+        messages = arrayListOf<Message>()
 
         edMessage.setOnKeyListener {v, keyCode, event ->
             if (keyCode == KeyEvent.KEYCODE_ENTER && event.action == KeyEvent.ACTION_UP) {
@@ -60,10 +65,17 @@ class MainActivity : AppCompatActivity() {
                 if (snapshot.value != null) {
                     val messagesFromDatabase= (snapshot.value as HashMap<String, ArrayList<String>>).get("messages")
                     messages.clear()
-                    messagesFromDatabase?.forEach {
-                        if (it != null) messages.add(it)
-                    }
+                        if (messagesFromDatabase != null) {
+                            for (i in 0.. messagesFromDatabase.size-1) {
+                                if (messagesFromDatabase.get(i) != null) {
+                                    val message: Message = Message.from(messagesFromDatabase.get(i) as HashMap<String, String>)
+                                    messages.add(message)
+                                }
+                            }
+                        }
+
                     rcMessageList.adapter?.notifyDataSetChanged()
+                    rcMessageList.smoothScrollToPosition(rcMessageList.adapter!!.itemCount -1)
                 }
             }
 
@@ -78,11 +90,13 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun addMessage() {
-        val newMessage = edMessage.text.toString()
+        val formatter: DateTimeFormatter = DateTimeFormatter. ofPattern("dd.MM.yyyy HH:mm")
+        val newMessage: Message = Message(edMessage.text.toString(), currentUser?.email.toString(),formatter.format(LocalDateTime.now()))
         messages.add(newMessage)
         database.child("messages").setValue(messages)
         edMessage.setText("")
         closeKeyBoard()
+        rcMessageList.smoothScrollToPosition(rcMessageList.adapter!!.itemCount -1)
     }
 
     private fun closeKeyBoard() {
@@ -93,10 +107,32 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater = menuInflater
+        inflater.inflate(R.menu.app_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem) = when (item.itemId) {
+        R.id.settings -> {
+            this.showSettings()
+            true
+        }else -> {
+            super.onOptionsItemSelected(item)
+        }
+    }
+
     override fun onStart() {
         super.onStart()
+        currentUser = auth.currentUser
+        if (currentUser == null) loginDialog()
+    }
 
-        loginDialog()
+    private fun showSettings() {
+        val intent = Intent(this, Settings::class.java).apply {
+            putExtra("currentUser", currentUser)
+        }
+        startActivity(intent)
     }
 
     private fun loginDialog() {
@@ -137,6 +173,7 @@ class MainActivity : AppCompatActivity() {
                     Toast.makeText(baseContext, "Authentication failed", Toast.LENGTH_SHORT).show()
                 }
             }
-
     }
+
+
 }
